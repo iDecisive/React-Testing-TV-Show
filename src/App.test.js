33e6c77676b0dfd64dd6b2645f2936a8f7e9faec
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-06-16 11:06:30 -0400

    Added App.js test

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {render} from '@testing-library/react';
+import App from './App';
+
+test('App.js renders and displays show title', () => {
+
+    const { rerender, getByText, findByText } = render(<App />);
+
+    const waitMsg = getByText(/Fetching data.../i); //getBy is not async
+
+    expect(waitMsg).toBeInTheDocument();
+
+    //const title = findByText(/Stranger Things/i); //findBy is async
+
+    //expect(title).toBeInTheDocument();
+
+})
